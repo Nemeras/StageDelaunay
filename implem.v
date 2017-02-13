1626eae2514793135a535d1a89fd611d9d4806cd
csartori <clementsartori35@gmail.com>
2017-02-13 11:41:28 +0100

    ajout de implem.v et avancees diverses

--- implem.v
+++ implem.v
@@ -1,5 +1,19 @@
-Definition isLeftOf := fun (p a b : P) =>
-  let M1 := \matrix_( i<3, j<3) if i==@Ordinal 3 0 isT then if j==@Ordinal 3 0 isT then xCoord p
+From mathcomp Require Import ssreflect ssrbool ssrnat eqtype ssrfun seq tuple.
+From mathcomp Require Import choice path finset finfun fintype bigop.
+From mathcomp Require Import ssralg ssrnum matrix mxalgebra.
+From mathcomp Require Import finmap.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Require Import triangulation.
+
+Open Local Scope ring_scope.
+
+
+Definition orientedSurface := fun (p a b : P) =>
+ let M1 := \matrix_( i<3, j<3) if i==@Ordinal 3 0 isT then if j==@Ordinal 3 0 isT then xCoord p
                                         else if j==@Ordinal 3 1 isT then yCoord p
                                                else 1%R
                              else if i==@Ordinal 3 1 isT then if j==@Ordinal 3 0 isT then xCoord a
@@ -9,4 +23,44 @@ Definition isLeftOf := fun (p a b : P) =>
                                        else if j==@Ordinal 3 1 isT then yCoord b
                                             else 1%R
   in \det M1.
-              
+
+
+Definition inCircle (p a b c : P) :=
+  let M:= \matrix_(i<4, j<4) if i == @Ordinal 4 0 isT then if j==@Ordinal 4 0 isT then
+                                     xCoord a
+                                         else if j==@Ordinal 4 1 isT  then
+                                     yCoord a
+                                         else if j== @Ordinal 4 2 isT  then
+                         (xCoord a)^+2
+                            + (yCoord a)^+2
+                                         else 1%R
+                           else if i == @Ordinal 4 1 isT  then if j== @Ordinal 4 0 isT  then
+                                     xCoord b
+                                         else if j==@Ordinal 4 1 isT  then
+                                     yCoord b
+                                         else if j== @Ordinal 4 2 isT  then 
+                         (xCoord b)^+2
+                            + (yCoord b)^+2
+                                         else 1%R
+                           else if i == @Ordinal 4 2 isT  then if j== @Ordinal 4 0 isT  then
+                                     xCoord c
+                                         else if j== @Ordinal 4 1 isT  then
+                                     yCoord c
+                                         else if j== @Ordinal 4 2 isT  then 
+                         (xCoord c)^+2
+                            + (yCoord c)^+2
+                                         else 1%R
+                           else if j== @Ordinal 4 0 isT  then
+                                     xCoord p
+                                         else if j== @Ordinal 4 1 isT  then
+                                     yCoord p
+                                         else if j== @Ordinal 4 2 isT  then 
+                                     (xCoord p)^+2 + (yCoord p)^+2 
+                                         else 1%R
+   in (\det M >0).
+
+Definition inCircleTriangle p t := inCircle p (vertex1 t) (vertex2 t) (vertex3 t).
+
+
+
+
--- triangulation.v
+++ triangulation.v
@@ -1,22 +1,21 @@
 From mathcomp Require Import ssreflect ssrbool ssrnat eqtype ssrfun seq tuple.
 From mathcomp Require Import choice path finset finfun fintype bigop.
 From mathcomp Require Import ssralg ssrnum matrix mxalgebra.
-
 From mathcomp Require Import finmap.
+
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
+Section Triangulation.
+        
 Open Scope fset_scope.
 
-Variable R : numDomainType.
-(*numDomainType.*)
+Parameter R : numDomainType.
+
 
 Parameter P : choiceType.
 Parameter T : choiceType.
-
-Check R.
-
 
 Parameter xCoord : P -> R.
 Parameter yCoord : P -> R.
@@ -27,10 +26,9 @@ Axiom not_2points : forall p1, forall p2, xCoord p1 = xCoord p2 -> yCoord p1 != 
 Parameter Tr : {fset T}.
 Parameter D : {fset P}.
 
-Parameter vertex : T ->P^3.
+Parameter vertex : T -> P^3.
 
 Variable tt:T.
-Check vertex tt .
 
 Parameter interior : T -> P -> bool.
 Parameter surface : T -> P -> bool.
@@ -39,28 +37,37 @@ Parameter orientedSurface : P -> P -> P -> R.
 (*Axiom triangle3Vertices : forall (t: T), #|` vertex t| = 3.*)
 Axiom triangle3Vertices : forall (t:T), injective (vertex t).
 
+
 Open Local Scope ring_scope.
-Definition vertexSet t := image (vertex t) 'I_3.
+Definition vertexSet t := (vertex t) @` 'I_3.
 Check vertexSet.
 Definition adjacent t1 t2:= #|` ((vertexSet t1) `&` (vertexSet t2))| = 2 /\ (forall p, interior t1 p -> interior t2 p = false).
 Definition isLeftOf p a b := orientedSurface p a b > 0%R.
+Check Ordinal.
+
+Definition vertex1 t := vertex t (@Ordinal 3 0 isT).
+Definition vertex2 t := vertex t (@Ordinal 3 1 isT).
+Definition vertex3 t := vertex t (@Ordinal 3 2 isT).
+
+Axiom orientedTriangle : forall t, orientedSurface (vertex1 t) (vertex2 t) (vertex3 t)>0. 
 
 (* à ameliorer : regarder le troisieme sommet ?*)
 
-Definition inTriangle t p := match 
+Definition inTriangle t p := isLeftOf (vertex1 t) (vertex2 t) p /\
+                             isLeftOf p (vertex2 t) (vertex3 t) /\
+                             isLeftOf (vertex1 t) p (vertex3 t).
 
-  : T -> P -> bool.
 
 Parameter inCircle :  T -> P -> bool.
 
-Axiom allTriangles : forall p1, forall p2, forall p3, exists t, p1 \in vertex t /\ p2 \in vertex t /\ p3 \in vertex t.
+Axiom allTriangles : forall p1, forall p2, forall p3, exists t, p1 \in vertexSet t /\ p2 \in vertexSet t /\ p3 \in vertexSet t.
 
 
 Definition  Delaunay (Ts:{fset T})  := forall t1 , forall t2,
       t1 \in Ts -> t2 \in Ts ->
                           adjacent t1 t2 ->
                           forall p,
-                           p \in vertex t1-> inCircle t2 p -> p \in vertex t2.
+                           p \in vertexSet t1-> inCircle t2 p -> p \in vertexSet t2.
 
 
 
@@ -68,9 +75,10 @@ Definition  Delaunay (Ts:{fset T})  := forall t1 , forall t2,
 (*Axiom union_TrD : \bigcup_ ( t in Tr ) (vertex t) = D.
 ??*)
 
-Definition unionTrD1 (Ts: {fset T}) (Ds : {fset P}) := forall (t:T), t \in Ts -> forall p, p \in vertex t -> p \in Ds. 
+Definition unionTrD1 (Ts: {fset T}) (Ds : {fset P}) := forall (t:T), t \in Ts -> forall p, p \in vertexSet t -> p \in Ds. 
 
-Definition unionTrD2 (Ts: {fset T}) (Ds : {fset P}) := forall (p:P), p \in Ds -> exists (t:T), t \in Ts /\ p \in vertex t.
+Definition unionTrD2 (Ts: {fset T}) (Ds : {fset P}) := forall (p:P), p \in Ds -> exists (t:T), t \in Ts /\ p \in vertexSet t.
 
 Definition unionTrD Ts Ds := unionTrD1 Ts Ds /\ unionTrD2 Ts Ds.
                                            
+End Triangulation.
